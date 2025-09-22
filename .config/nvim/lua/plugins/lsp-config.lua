e480591366d85002ff405b52a2c3110b9d160798
Ravieroy <ravieroy123@gmail.com>
2025-09-22 17:55:37 +0530

    019: Formatting, update lsp-config for nvim v0.11.4 changes, enable shortcut for zen mode

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,38 +1,82 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
+		opts = {},
 	},
+
 	{
 		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "julials", "pylsp" },
-				automatic_enable = false,
-			})
-		end,
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"julials",
+				"pylsp",
+				"marksman", -- Markdown
+				"ltex", -- grammar/spell-check
+				"typos_lsp", -- typo/spell-check
+			},
+			automatic_enable = false, -- keep manual control
+		},
 	},
+
 	{
 		"neovim/nvim-lspconfig",
 		lazy = false,
-		config = function()
+		opts = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
 
-			-- Set up language servers
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.julials.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pylsp.setup({
-				capabilities = capabilities,
-			})
+			return {
+				-- global defaults applied to all servers
+				["*"] = {
+					capabilities = capabilities,
+				},
 
-			-- Diagnostic display settings
+				-- per-server configs
+				lua_ls = {
+					settings = {
+						Lua = {
+							diagnostics = { globals = { "vim" } },
+						},
+					},
+				},
+
+				julials = {},
+
+				pylsp = {},
+
+				marksman = {},
+
+				ltex = {
+					settings = {
+						ltex = {
+							language = "en-US",
+							enabled = { "markdown", "text", "tex" },
+							dictionary = {
+								["en-US"] = { "Neovim", "lua_ls", "mason" },
+							},
+							disabledRules = { ["en-US"] = { "EN_QUOTES" } },
+						},
+					},
+				},
+
+				typos_lsp = {},
+			}
+		end,
+		config = function(_, opts)
+			-- Register configs
+			for server, server_opts in pairs(opts) do
+				vim.lsp.config(server, server_opts)
+			end
+
+			-- Manually enable servers (since automatic_enable = false)
+			vim.lsp.enable("lua_ls")
+			vim.lsp.enable("julials")
+			vim.lsp.enable("pylsp")
+			vim.lsp.enable("marksman")
+			vim.lsp.enable("ltex")
+			vim.lsp.enable("typos_lsp")
+
+			-- Diagnostics config
 			vim.diagnostic.config({
 				virtual_text = false,
 				underline = true,
@@ -41,14 +85,21 @@ return {
 				severity_sort = true,
 			})
 
-			-- Show floating diagnostics on hover
-			-- vim.o.updatetime = 250
-			-- vim.cmd([[autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })]])
-
-			-- LSP key mappings
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			-- Buffer-local keymaps
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("user_lsp_config", { clear = true }),
+				callback = function(args)
+					local bufnr = args.buf
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP: Hover" })
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "LSP: Definition" })
+					vim.keymap.set(
+						{ "n", "v" },
+						"<leader>ca",
+						vim.lsp.buf.code_action,
+						{ buffer = bufnr, desc = "LSP: Code Action" }
+					)
+				end,
+			})
 		end,
 	},
 }
