dd9a9c1eef0fa6fda21e3b1e1d4d0dd40386cae1
Ravieroy <ravieroy123@gmail.com>
2026-05-21 16:19:32 +0530

    036: Updated yazi config

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,5 +1,5 @@
--- require("full-border"):setup()
-require("full-border"):setup {
-	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-	type = ui.Border.ROUNDED,
+require("full-border"):setup()
+
+require("git"):setup {
+  order = 1500,
 }
